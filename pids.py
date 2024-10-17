78b3303ca80fafde56a9a50e4da5534ebc86c21a
Jukka-Pekka Keskinen <jukka-pekka.keskinen@fmi.fi>
2024-10-17 17:09:22 +0300

    Lisätty 3D-PAD-tiedon lukeminen.

--- pids.py
+++ pids.py
@@ -59,7 +59,7 @@ class Pids:
                 else:
                     os.exit('CRS-tiedoista puuttuu '+i)                                
 
-            # Liukulukumoitoiset
+            # Liukulukumuotoiset
             for i in ['semi_major_axis', 'inverse_flattening',
                       'longitude_of_prime_meridian',
                       'longitude_of_central_meridian',
@@ -75,20 +75,68 @@ class Pids:
 
         # Luetaan maanpintatiedot
         if 'dem' in data:
-             D = rxr.open_rasterio(data['dem'])
-             D = D.isel(band=0).sortby('y')
-             if not 'x' in self.xrds.coords and not 'y' in self.xrds.coords:
-                 self.xrds['zt'] = xr.DataArray( D.data, coords = [
-                     ('y', (D.y-D.y[0]).data.astype(np.float32)),
-                     ('x', (D.x-D.x[0]).data.astype(np.float32)) ] )
-             else:
-                 os.exit('Koordinaatistot ovat jo olemassa.')
+            D = rxr.open_rasterio(data['dem'])
+            D = D.isel(band=0).sortby('y')
+            if not 'x' in self.xrds.coords and not 'y' in self.xrds.coords:
+                self.xrds['zt'] = xr.DataArray( D.data, coords = [
+                    ('y', (D.y-D.y[0]).data.astype(np.float32)),
+                    ('x', (D.x-D.x[0]).data.astype(np.float32)) ] )
+            else:
+                os.exit('Koordinaatistot ovat jo olemassa.')
+
+            D.close()
+            del D
 
         # Luetaan latvustotiedot. Siirtymä 2D-rasterista 3D:hen
         # tapahtuu samoin kuin P4UL:ssa.
         if 'chm' in data:
-               
-             
+            print('2D-latvustotietoja ei ole vielä toteutettu.')
+            os.exit()
+
+        if '3dpad' in data:
+            if 'tiedosto' in data['3dpad']:
+                D = rxr.open_rasterio(data['3dpad']['tiedosto'])
+            else:
+                os.exit('3D-tiedoston nimi puuttuu.')
+
+            # Oletetaan, että luettavassa tiedostossa ensimmäine  taso on 0 m.
+            if 'dztiff' in data['3dpad']:
+                dz0 = float(data['3dpad']['dztiff'])
+            else:
+                dz0 = 1.0
+            
+            if 'x' in self.xrds.coords and 'y' in self.xrds.coords:
+                if self.xrds.x.size == D.x.size and self.xrds.y.size == D.y.size:
+                    T = xr.DataArray( D.data, coords = [
+                        ('zlad', dz0*(D.band.data-1).astype(np.float32)),
+                        ('y', self.xrds.y), ('x', self.xrds.x) ] )
+                else:
+                    os.exit('Koordinaatistot eivät täsmää.')
+            else:
+                T = xr.DataArray( D.data, coords = [
+                    ('zlad', dz0*(D.band.data-1).astype(np.float32)),
+                    ('y', (D.y-D.y[0]).data.astype(np.float32)),
+                    ('x', (D.x-D.x[0]).data.astype(np.float32))  ] )
+
+            # Muokataan z-akseli kuntoon.
+            if 'dzpids' in data['3dpad']:
+                dz = float(data['3dpad']['dzpids'])
+            else:
+                dz = dz0
+
+            if 'interpolointi' in data['3dpad']:
+                inmen = data['3dpad']['interpolointi']
+            else:
+                inmen = 'nearest'
+
+            zu = np.append(0.0, np.arange(dz/2, T.zlad[-1]+dz/100, dz, dtype=np.float32) )
+
+            self.xrds['lad'] = T.interp(zlad=zu, method=inmen)
+
+            D.close()
+            del D
+            del T
+            
     def tallennus(self, polku='PIDS_STATIC'):
         self.xrds.attrs['history'] = str(datetime.now().replace(microsecond=0)) + ': File created'
         self.xrds.attrs['creation_time'] = str(
