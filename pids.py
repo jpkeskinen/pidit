ff9000c3e0be4229a5585ef7640a2fbaacdd16df
Jukka-Pekka Keskinen <jukka-pekka.keskinen@fmi.fi>
2024-11-14 14:44:28 +0200

    2D- ja 3D-latvustotiedot mukaan.

--- pids.py
+++ pids.py
@@ -7,8 +7,9 @@ import xarray as xr
 import rioxarray as rxr
 from datetime import datetime, timezone
 import yaml
-import os
+import sys
 import numpy as np
+
 
 class Pids:
     def __init__(self):
@@ -57,7 +58,7 @@ class Pids:
                 if i in data['crs']:
                     CRS.attrs[i] = str(data['crs'][i])
                 else:
-                    os.exit('CRS-tiedoista puuttuu '+i)                                
+                    sys.exit('CRS-tiedoista puuttuu '+i)                                
 
             # Liukulukumuotoiset
             for i in ['semi_major_axis', 'inverse_flattening',
@@ -69,7 +70,7 @@ class Pids:
                 if i in data['crs']:
                     CRS.attrs[i] = float(data['crs'][i])
                 else:
-                    os.exit('CRS-tiedoista puuttuu '+i)                                
+                    sys.exit('CRS-tiedoista puuttuu '+i)                                
 
             self.xrds['crs'] = CRS
 
@@ -82,7 +83,7 @@ class Pids:
                     ('y', (D.y-D.y[0]).data.astype(np.float32)),
                     ('x', (D.x-D.x[0]).data.astype(np.float32)) ] )
             else:
-                os.exit('Koordinaatistot ovat jo olemassa.')
+                sys.exit('Koordinaatistot ovat jo olemassa.')
 
             D.close()
             del D
@@ -90,18 +91,47 @@ class Pids:
         # Luetaan latvustotiedot. Siirtymä 2D-rasterista 3D:hen
         # tapahtuu samoin kuin P4UL:ssa.
         if 'chm' in data:
-            print('2D-latvustotietoja ei ole vielä toteutettu.')
-            os.exit()
+            if 'tiedosto' in data['chm']:
+                R = rxr.open_rasterio(data['chm']['tiedosto'])
+                dx = np.abs([R.x[2].data-R.x[1].data, R.y[2].data-R.y[1].data])
+                if 'dz' in data['chm']:
+                    dz = float(data['chm']['dz'])
+                else:
+                    dz = dx[0]
+                D = _chm_p4ul(R.isel(band=0).sortby('y').data,dx,dz=dz)
 
-        if '3dpad' in data:
-            if 'tiedosto' in data['3dpad']:
-                D = rxr.open_rasterio(data['3dpad']['tiedosto'])
+                if 'x' in self.xrds.coords and 'y' in self.xrds.coords:
+                    if self.xrds.x.size == R.x.size and self.xrds.y.size == R.y.size:
+                        xk = self.xrds.x
+                        yk = self.xrds.y
+                    else:
+                        sys.exit('Koordinaatistot eivät täsmää.')
+                else:
+                    xk = (R.x-R.x[0]).data.astype(np.float32)
+                    yk = (R.y-R.y[0]).data.astype(np.float32)
+                    
+                T = xr.DataArray( D, coords = [ ('x', xk), ('y', yk),
+                            ('zlad', dz*(np.arange(D.shape[-1])).astype(np.float32)) ] )
+
+                R.close()
+                del R
+                del D
+                
             else:
-                os.exit('3D-tiedoston nimi puuttuu.')
+                print('2D-tiedoston nimi puuttuu.')
+                sys.exit()
+                 
+                
+        
+        if 'tiedosto3d' in data['chm']:
+            print('Luodaan latvustotiedot 3D-tiedostosta.')
+            D = rxr.open_rasterio(data['3dpad']['tiedosto'])
 
             # Oletetaan, että luettavassa tiedostossa ensimmäine  taso on 0 m.
-            if 'dztiff' in data['3dpad']:
-                dz0 = float(data['3dpad']['dztiff'])
+            if 'dztiff' in data['chm']:
+                dz0 = float(data['chm']['dztiff'])
+            elif 'dz' in data['chm']:
+                dz0 = float(data['chm']['dz'])
             else:
                 dz0 = 1.0
             
@@ -111,7 +141,7 @@ class Pids:
                         ('zlad', dz0*(D.band.data-1).astype(np.float32)),
                         ('y', self.xrds.y), ('x', self.xrds.x) ] )
                 else:
-                    os.exit('Koordinaatistot eivät täsmää.')
+                    sys.exit('Koordinaatistot eivät täsmää.')
             else:
                 T = xr.DataArray( D.data, coords = [
                     ('zlad', dz0*(D.band.data-1).astype(np.float32)),
@@ -119,26 +149,78 @@ class Pids:
                     ('x', (D.x-D.x[0]).data.astype(np.float32))  ] )
 
             # Muokataan z-akseli kuntoon.
-            if 'dzpids' in data['3dpad']:
-                dz = float(data['3dpad']['dzpids'])
+            if 'dz' in data['chm']:
+                dz = float(data['chm']['dz'])
             else:
                 dz = dz0
 
-            if 'interpolointi' in data['3dpad']:
-                inmen = data['3dpad']['interpolointi']
-            else:
-                inmen = 'nearest'
-
-            zu = np.append(0.0, np.arange(dz/2, T.zlad[-1]+dz/100, dz, dtype=np.float32) )
-
-            self.xrds['lad'] = T.interp(zlad=zu, method=inmen)
-
             D.close()
             del D
-            del T
+                
+        if 'interpolointi' in data['chm']:
+            inmen = data['chm']['interpolointi']
+        else:
+            inmen = 'nearest'
+                
+        zu = np.append(0.0, np.arange(dz/2, T.zlad[-1]+dz/100, dz, dtype=np.float32) )
+        
+        self.xrds['lad'] = T.interp(zlad=zu, method=inmen)
+
+        del T
+
+    
+
             
     def tallennus(self, polku='PIDS_STATIC'):
         self.xrds.attrs['history'] = str(datetime.now().replace(microsecond=0)) + ': File created'
         self.xrds.attrs['creation_time'] = str(
             datetime.now(timezone.utc).replace(microsecond=0))[:-6] + ' +00'
         self.xrds.to_netcdf(polku)
+
+def _chm_p4ul(R, dPx, laiRef=6.0, zref=[4.0, 20.0], dz=None):
+    # The function code is adapted from P4UL
+    # The code is licensed under the MIT License.
+    # Copyright (c) 2017 Mikko J.S. Auvinen
+    
+    nPx = np.shape(R)
+
+    # Calculate the shape of the new 3D array, use largest in-canopy value
+    nPx3D = nPx; dPx3D = dPx
+    if ( dz ):
+        dPx3D = np.append(dPx, dz)
+    else:
+        dPx3D = np.append(dPx, dPx[0])
+
+    nPx3D = np.append(nPx3D, int(np.floor(np.amax(R)/dPx3D[2])+1))
+
+    # Fill the 3D canopy array
+    canopy = np.zeros([nPx3D[1], nPx3D[0], nPx3D[2]])
+    nPc=[nPx3D[1], nPx3D[0], nPx3D[2]]
+    dPc=[dPx3D[1], dPx3D[0], dPx3D[2]]
+    print(" 3D grid array dimensions [x,y,z]: {}, {}, {}".format(*nPc))
+    print(" Generating vertical distributions of leaf area densities ...")
+
+
+    # Compute <LAD>_z and starting index of the foliage using reference values
+    lad_const = laiRef/(zref[1]-zref[0])
+    k1        = int( np.round(zref[0]/float(dPc[2])) )  # starting k index
+
+    print(' Rry shape = {} '.format(R.shape))
+
+    # Calculate leaf area density profiles for each horizontal grid tile and fill array vertically
+    for j in range(nPc[1]):
+        for i in range(nPc[0]):
+            Zi = R[j,i] # Zi := canopy height at [j,i]
+            # Check if there is canopy at all in the vertical column
+            if (Zi <= zref[0]):
+                continue
+
+            # Number of layers
+            dZ   = Zi - zref[0]
+            nind = int(np.floor( dZ/float(dPc[2]) )) + 1
+
+            k2 = int(np.ceil(Zi/dPc[2]))+1
+            k2 = min( k2, nPc[2] )
+            canopy[i,j,k1:k2] = lad_const
+
+    return canopy
